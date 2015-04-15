d01e2d217a9e934088e3706d19b356f9c7e5f04c
Jerry Kuru <jerrykuru@gmail.com>
2015-04-15 15:39:08 -0400

    Basic Setup

---
+++ src/test/scala/demo/helloTest.scala
@@ -0,0 +1,11 @@
+package demo
+
+import org.scalatest.FunSuite
+
+
+class helloTest extends FunSuite {
+  test("say HelloMethod works correctly") {
+    val hello = new Hello
+    assert(hello.sayHello("Scala") == "Hello, Scala")
+  }
+}
