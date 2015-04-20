283789b21e0924ef524c0bdd549f834191b257cd
Jerry Kuru <jerrykuru@gmail.com>
2015-04-20 10:24:18 -0400

    Added Fibonacci , the test can only be run from SBT

---
+++ src/main/scala/demo/HighOrderFunction.scala
@@ -0,0 +1,8 @@
+package demo
+
+/**
+ * Created by binu on 4/20/15.
+ */
+class HighOrderFunction {
+
+}
---
+++ src/test/scala/demo/FibnonacciNewTest.scala
@@ -0,0 +1,10 @@
+package demo
+
+import org.scalatest.FunSuite
+
+/**
+ * Created by binu on 4/20/15.
+ */
+class FibnonacciNewTest extends FunSuite {
+
+}
